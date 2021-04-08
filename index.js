2686aaf60e2fc1c8862efd71c7937ff499be5eb8
jihye-at-349545901023 <byjihye@hashsnap.net>
2021-04-08 10:20:19 +0900

    크리티컬 섹션

---
+++ index.js
@@ -0,0 +1,12 @@
+class Stack {
+  stack;
+  top = 0;
+  MAX_SIZE = 5;
+  constructor() {
+    this.stack = Array.from({ length: this.MAX_SIZE });
+  }
+  pop(item) {}
+  push(item) {}
+  empty() {}
+  size() {}
+}
